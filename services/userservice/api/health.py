d9f0f4f16a69d79561b88e2fc5f89fe5ddb02b39
변성훈 <gd010123@gmail.com>
2026-07-09 14:26:14 +0900

    Update health.py

--- services/userservice/api/health.py
+++ services/userservice/api/health.py
@@ -1,23 +1,100 @@
 from flask import Blueprint, jsonify
-from config.config import Config
 
-health_bp = Blueprint("health", __name__)
+from config.database import get_connection
+from config.redis import redis_client
+
+from config.kafka import create_producer
 
 
-@health_bp.route("/health", methods=["GET"])
+
+health_bp = Blueprint(
+    "health",
+    __name__
+)
+
+
+
+
+
+@health_bp.route(
+    "/health",
+    methods=["GET"]
+)
 def health():
-    # Kubernetes가 Pod 상태를 확인할 때 사용할 API
-    return jsonify({
-        "status": "ok",
-        "service": Config.SERVICE_NAME
-    })
 
 
-@health_bp.route("/version", methods=["GET"])
-def version():
-    # GitLab CI/CD 배포 후 현재 실행 중인 버전을 확인할 API
-    return jsonify({
-        "service": Config.SERVICE_NAME,
-        "version": Config.APP_VERSION,
-        "env": Config.APP_ENV
-    })
+    result = {
+
+        "service":
+        "SmartWMS UserService",
+
+        "status":
+        "running"
+
+    }
+
+
+
+    # PostgreSQL 확인
+
+    try:
+
+        conn = get_connection()
+
+        conn.close()
+
+
+        result["postgresql"] = "ok"
+
+
+    except Exception:
+
+
+        result["postgresql"] = "error"
+
+
+
+
+
+    # Redis 확인
+
+    try:
+
+        redis_client.ping()
+
+        result["redis"] = "ok"
+
+
+    except Exception:
+
+
+        result["redis"] = "error"
+
+
+
+
+
+    # Kafka 확인
+
+    try:
+
+        producer = create_producer()
+
+        producer.close()
+
+
+        result["kafka"] = "ok"
+
+
+    except Exception:
+
+
+        result["kafka"] = "error"
+
+
+
+
+
+    return jsonify(
+        result
+    )
