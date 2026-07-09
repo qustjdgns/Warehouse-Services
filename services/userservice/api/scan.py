88d4eb5966ea8f04468d0b3e1bdccad1f3e6c6b6
변성훈 <gd010123@gmail.com>
2026-07-09 12:23:57 +0900

    Update scan.py

--- services/userservice/api/scan.py
+++ services/userservice/api/scan.py
@@ -2,12 +2,23 @@ from flask import Blueprint, request, jsonify
 from services.scan_service import validate_barcode
 from services.product_service import get_product
 
+from kafka import KafkaProducer
+import json
+
+
 scan_bp = Blueprint("scan", __name__)
+
+
+# Kafka Producer 설정
+producer = KafkaProducer(
+    bootstrap_servers=["kafka-service:9092"],
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
 
 
 @scan_bp.route("/scan", methods=["POST"])
 def scan():
-    # 사용자가 보낸 JSON 데이터 받기
+
     data = request.get_json()
 
     if data is None:
@@ -15,10 +26,11 @@ def scan():
             "error": "JSON body가 필요합니다."
         }), 400
 
-    # barcode 값 추출
+
     barcode = data.get("barcode")
 
-    # barcode 값 검증
+
+    # barcode 검증
     is_valid, message = validate_barcode(barcode)
 
     if not is_valid:
@@ -26,16 +38,35 @@ def scan():
             "error": message
         }), 400
 
-    
+
+    # 상품 조회
     product = get_product(barcode)
-  
+
     if product is None:
         return jsonify({
             "error": "상품을 찾을 수 없습니다."
         }), 404
 
 
+    # Kafka 이벤트 발행
+    event = {
+        "event_type": "SCAN",
+        "barcode": barcode,
+        "product": product
+    }
+
+
+    producer.send(
+        "scan-event",
+        event
+    )
+
+
+    producer.flush()
+
+
     return jsonify({
-        "barcode" : barcode,
-        "product" : product
-        })
+        "message": "스캔 이벤트 발행 완료",
+        "barcode": barcode,
+        "product": product
+    })
